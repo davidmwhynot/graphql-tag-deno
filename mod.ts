e3497daa01b3c2df814e655895fa5d2be873d39a
David Whynot <dwhynot@biggby.com>
2020-05-24 16:57:07 -0400

    setup graphql-tag-deno

---
+++ mod.ts
@@ -0,0 +1,3 @@
+import gql from './src/index.js';
+
+export default gql;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-var parser = require('graphql/language/parser');
-
-var parse = parser.parse;
+import { parse } from "https://raw.githubusercontent.com/adelsz/graphql-deno/v15.0.0/mod.ts";
 
 // Strip insignificant whitespace
 // Note that this could do a lot more, such as reorder fields etc.
 function normalize(string) {
-  return string.replace(/[\s,]+/g, ' ').trim();
+  return string.replace(/[\s,]+/g, " ").trim();
 }
 
 // A map docString -> graphql document
@@ -35,23 +33,28 @@ function processFragments(ast) {
   for (var i = 0; i < ast.definitions.length; i++) {
     var fragmentDefinition = ast.definitions[i];
 
-    if (fragmentDefinition.kind === 'FragmentDefinition') {
+    if (fragmentDefinition.kind === "FragmentDefinition") {
       var fragmentName = fragmentDefinition.name.value;
       var sourceKey = cacheKeyFromLoc(fragmentDefinition.loc);
 
       // We know something about this fragment
-      if (fragmentSourceMap.hasOwnProperty(fragmentName) && !fragmentSourceMap[fragmentName][sourceKey]) {
-
+      if (
+        fragmentSourceMap.hasOwnProperty(fragmentName) &&
+        !fragmentSourceMap[fragmentName][sourceKey]
+      ) {
         // this is a problem because the app developer is trying to register another fragment with
         // the same name as one previously registered. So, we tell them about it.
         if (printFragmentWarnings) {
-          console.warn("Warning: fragment with name " + fragmentName + " already exists.\n"
-            + "graphql-tag enforces all fragment names across your application to be unique; read more about\n"
-            + "this in the docs: http://dev.apollodata.com/core/fragments.html#unique-names");
+          console.warn(
+            "Warning: fragment with name " +
+              fragmentName +
+              " already exists.\n" +
+              "graphql-tag enforces all fragment names across your application to be unique; read more about\n" +
+              "this in the docs: http://dev.apollodata.com/core/fragments.html#unique-names"
+          );
         }
 
         fragmentSourceMap[fragmentName][sourceKey] = true;
-
       } else if (!fragmentSourceMap.hasOwnProperty(fragmentName)) {
         fragmentSourceMap[fragmentName] = {};
         fragmentSourceMap[fragmentName][sourceKey] = true;
@@ -77,14 +80,14 @@ function disableFragmentWarnings() {
 function stripLoc(doc, removeLocAtThisLevel) {
   var docType = Object.prototype.toString.call(doc);
 
-  if (docType === '[object Array]') {
+  if (docType === "[object Array]") {
     return doc.map(function (d) {
       return stripLoc(d, removeLocAtThisLevel);
     });
   }
 
-  if (docType !== '[object Object]') {
-    throw new Error('Unexpected input.');
+  if (docType !== "[object Object]") {
+    throw new Error("Unexpected input.");
   }
 
   // We don't want to remove the root loc field so we can use it
@@ -109,7 +112,7 @@ function stripLoc(doc, removeLocAtThisLevel) {
       value = doc[keys[key]];
       valueType = Object.prototype.toString.call(value);
 
-      if (valueType === '[object Object]' || valueType === '[object Array]') {
+      if (valueType === "[object Object]" || valueType === "[object Array]") {
         doc[keys[key]] = stripLoc(value, true);
       }
     }
@@ -126,9 +129,11 @@ function parseDocument(doc) {
     return docCache[cacheKey];
   }
 
-  var parsed = parse(doc, { experimentalFragmentVariables: experimentalFragmentVariables });
-  if (!parsed || parsed.kind !== 'Document') {
-    throw new Error('Not a valid GraphQL document.');
+  var parsed = parse(doc, {
+    experimentalFragmentVariables: experimentalFragmentVariables,
+  });
+  if (!parsed || parsed.kind !== "Document") {
+    throw new Error("Not a valid GraphQL document.");
   }
 
   // check that all "new" fragments inside the documents are consistent with
@@ -155,10 +160,10 @@ function gql(/* arguments */) {
   var literals = args[0];
 
   // We always get literals[0] and then matching post literals for each arg given
-  var result = (typeof(literals) === "string") ? literals : literals[0];
+  var result = typeof literals === "string" ? literals : literals[0];
 
   for (var i = 1; i < args.length; i++) {
-    if (args[i] && args[i].kind && args[i].kind === 'Document') {
+    if (args[i] && args[i].kind && args[i].kind === "Document") {
       result += args[i].loc.source.body;
     } else {
       result += args[i];
@@ -177,4 +182,4 @@ gql.disableFragmentWarnings = disableFragmentWarnings;
 gql.enableExperimentalFragmentVariables = enableExperimentalFragmentVariables;
 gql.disableExperimentalFragmentVariables = disableExperimentalFragmentVariables;
 
-module.exports = gql;
+export default gql;

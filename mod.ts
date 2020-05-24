57d4a0fc755556833d5b9d5b05bfd9bae307bdd7
David Whynot <dwhynot@biggby.com>
2020-05-24 17:21:38 -0400

    add types to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,6 @@
-import gql from './src/index.js';
+import { DocumentNode } from 'https://raw.githubusercontent.com/adelsz/graphql-deno/v15.0.0/mod.ts';
+import gqlTag from './src/index.js';
+
+const gql: (literals: any, ...placeholders: any[]) => DocumentNode = gqlTag;
 
 export default gql;
